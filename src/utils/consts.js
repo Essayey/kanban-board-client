391f1046409b301d62d3becfdb92fbc6654603b8
Essayey <47187299+Essayey@users.noreply.github.com>
2022-10-17 17:39:10 +0300

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -7,7 +7,7 @@ export const CARD_MODAL_ROUTE = '/boards/:id/:cardId'
 export const INVITE_ROUTE = '/invite/:token'
 export const CARDS_ROUTE = '/cards'
 
-export const SERVER_URL = 'http://45.80.68.107:5000/'
+export const SERVER_URL = 'https://45.80.68.107:5000/'
 
 export const colors = ["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4",
     "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722",
